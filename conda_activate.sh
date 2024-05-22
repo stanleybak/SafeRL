c41bc5db644473ca2e29563bdb14be65e41a1bd7
Stanley Bak <stanleybak@gmail.com>
2024-05-22 15:46:49 -0400

    Added instructions to make plot

--- conda_activate.sh
+++ conda_activate.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# setup:
 # conda create -n rlenv python=3.9
 #
-# after you make conda environment:
+# activate conda:
+# source conda_activate.sh
 #
-# pip install setuptools==65.5.1 pip==21.0 wheel==0.38.0
+# after you activate the conda environment:
+#
 # pip install -r requirements.txt
-# (you may still get a pip warning about versions)
 # try 'python3 test_libs.py' and see if it runs
+# 
+# training: 
 # then do: python scripts/train.py --config configs/rejoin/rejoin_default.yaml
+#
+# need to modify these commands based on the output path date:
+# produces eval.log for plotting:
+# python3 scripts/eval.py --dir /home/stan/Desktop/repositories/SafeRL/output/expr_20240522_143535/ --num_rollouts 1
+#
+# custom plotting code to make eval_plot.png file:
+# python3 scripts/plot_stan.py output/expr_20240522_143535/PPO_DubinsRejoin_15bc3_00000_0_2024-05-22_14-35-38/eval/ckpt_200/eval.log
  
 conda activate rlenv
